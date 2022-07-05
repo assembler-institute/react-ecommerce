14f369cfb5cbd854f4b5e2dcb4e876c60357a4ff
Javier Fernandez <Dogsouldev@gmail.com>
2022-07-05 12:40:23 +0200

    add Produc.css

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,14 +2,18 @@ import './Product.css'
 
 const Product = () => {
   return (
-    <div>
-      <span>56%</span>
-      [12:13 PM] Alejandro Avila
-     <img src="https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2Fflash-3.png&w=3840&q=75" alt="img" />
-     <p>Samsung</p>
-      <span>⭐️⭐️⭐️⭐️⭐️</span>
-      <p>$80</p>
-      <button></button>
+    <div className='product__container'>
+      <span className='product__container_discount'>56%</span>
+     <img className='product__container_img' src="https://bonik-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2Fflash-3.png&w=3840&q=75" alt="img" />
+     <p className='product__container_title'>Samsung</p>
+      <span className='product__container_rating'>⭐️⭐️⭐️⭐️⭐️</span>
+      <p className='product__container_description'>Description</p>
+
+      <div className='product__bottom'>
+      <p className='product__bottom_price'>$80</p>
+      <button className='product__bottom_buttonAdd'>Add</button>
+      </div>
+
     </div>
   )
 }
