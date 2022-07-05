d1db51c1bfce1e533900cd7ce8a2dbdbe046eb27
Javier Fernandez <Dogsouldev@gmail.com>
2022-07-05 23:59:19 +0200

    feat: :sparkles: "add ShoppingCart components"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
+import { useState } from "react";
 import Product from "./components/Product.jsx";
+import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
 import { products } from "./data/products";
 
 function App() {
+  const [buyShoppingCart, setBuyShoppingCart] = useState([]);
+
   console.log(products);
   return (
     <>
-      <div className="">
+      <div className="main__app">
+        <div className="main__products">
         <h1>Bruce Lee!</h1>
+        <div className="products_list">
         {products.map((item, index) => {
           return (
             <Product
@@ -18,9 +24,14 @@ function App() {
               price={item.price}
               description={item.description}
               image={item.image}
+              cart={buyShoppingCart}
+              setCart={setBuyShoppingCart}
             />
           );
         })}
+        </div>
+        </div>
+        <ShoppingCart cart={buyShoppingCart} setCart={setBuyShoppingCart}/>
       </div>
     </>
   );
--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -2,9 +2,14 @@ import "./ShoppingCart.css"
 
 const ShoppingCart = () => {
   return (
-    <div>
+    <div className="main__cart">
         <h3>ShoppingCart</h3>
-
+<ul className="cart__list"></ul>
+<div className="cart__buttom">
+    <p className="cart__total">Total:</p>
+    <p className="cart__totalPrice">$0</p>
+</div>
+<button>Checkout</button>
     </div>
   )
 }
