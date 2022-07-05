4de63899f267e51e4ffa0a1f3bd1acbe0f42603d
Javier Fernandez <Dogsouldev@gmail.com>
2022-07-05 11:31:59 +0200

    add data

--- src/data/products.js
+++ src/data/products.js
@@ -1,1 +1,42 @@
-export const products = []
+export const products = [
+    {
+title:'T-Shirt',
+price: 20,
+amount: 50,
+desription:'',
+image:'https://www.mistertee.net/images/1x1.png',
+valoration: 8,
+    },
+    {
+title:'Poster',
+price: 20,
+amount: 50,
+desription:'',
+image:'https://img2.freepng.es/20171217/f54/bruce-lee-png-5a36a3ca1b5217.8431159015135303141119.jpg',
+valoration: 8,
+    },
+    {
+title:'Poster',
+price: 20,
+amount: 50,
+desription:'',
+image:'https://img2.freepng.es/20171217/f54/bruce-lee-png-5a36a3ca1b5217.8431159015135303141119.jpg',
+valoration: 8,
+    },
+    {
+title:'Poster',
+price: 20,
+amount: 50,
+desription:'',
+image:'https://img2.freepng.es/20171217/f54/bruce-lee-png-5a36a3ca1b5217.8431159015135303141119.jpg',
+valoration: 8,
+    },
+    {
+title:'Poster',
+price: 20,
+amount: 50,
+desription:'',
+image:'https://img2.freepng.es/20171217/f54/bruce-lee-png-5a36a3ca1b5217.8431159015135303141119.jpg',
+valoration: 8,
+    },
+];
