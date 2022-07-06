f4d1b392be2e5834be3a36657de72d431ce79045
Jose1i1o <guadix88@gmail.com>
2022-07-06 10:17:29 +0200

    rebase head to previous commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import Products from './components/Products';
-// import Article from './components/Article';
 
-
-function App(props) {
-
+function App() {
   return (
-      // <Article {...props}/>
-    <>
-      <Products  />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
