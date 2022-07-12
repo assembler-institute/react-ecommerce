27b23589177b899b227c41b99adfba129008cf22
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-07-12 23:26:47 +0100

    feat: :sparkles: get data from products API

--- src/components/Store/Catalog/Catalog.jsx
+++ src/components/Store/Catalog/Catalog.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import Product from "../Product/Product";
 
 import "./Catalog.css";
 
 const Catalog = () => {
+  const [products, setSaveProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("http://localhost:3001/products");
+      const data = await response.json();
+      setSaveProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <div className="catalog__container">
-      <Product />
+      {products.map(({ id, title, price, image }) => {
+        return <Product key={id} title={title} price={price} image={image} />;
+      })}
     </div>
   );
 };
