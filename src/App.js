e39ccc6757d90ae5075043ead8579f878c27bd6d
Javier Fernández <dogsouldev@gmail.com>
2022-07-14 17:37:22 +0200

    add localStorage to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-// * Main Project Component.
-//? Here we renderize Navbar and Catalog Components and useEffect,useState hooks from React Library.
-
 import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Catalog from "./components/Store/Catalog/Catalog";
 
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 
-const initialShoppingCart =
-	JSON.parse(localStorage.getItem("shoppingCart")) || [];
+const initialStorage = JSON.parse(localStorage.getItem("saveCache")) || [];
 
 function App() {
-	const [shoppingCart, setShoppingCart] = useState([]);
+	const [shoppingCart, setShoppingCart] = useState(initialStorage);
+
+	useEffect(() => {
+		localStorage.setItem("saveCache", JSON.stringify(shoppingCart));
+	}, [shoppingCart]);
 
 	return (
 		<div className='main__app'>
 			<Navbar shoppingCart={shoppingCart} setShoppingCart={setShoppingCart} />
 			<Catalog shoppingCart={shoppingCart} setShoppingCart={setShoppingCart} />
+			<Footer />
 		</div>
 	);
 }
