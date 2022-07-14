bd1531dcc5dc5aa902bf5121b101166905fbf906
Javier Fernández <dogsouldev@gmail.com>
2022-07-14 13:21:00 +0200

    added useEffect((),shoppingCart.length,setTotalItemPrice

--- src/components/Cart/ShoppingCart/ShoppingCart.jsx
+++ src/components/Cart/ShoppingCart/ShoppingCart.jsx
@@ -7,61 +7,46 @@ import { Button } from "@nextui-org/react";
 import "./ShoppingCart.css";
 
 const ShoppingCart = ({ shoppingCart, setShoppingCart }) => {
-	const [total, setTotal] = useState(0);
+	const [totalItemPrice, setTotalItemPrice] = useState(0);
 
-	// useEffect(() => {
-	// 	let totalPrice = 0;
+	useEffect(() => {
+		let sumTotalItemPrice = 0;
 
-	// 	cart.forEach((item) => {
-	// 		totalPrice += item.cartPrice * item.amount;
-	// 	});
+		shoppingCart.forEach((item) => {
+			sumTotalItemPrice += item.itemPrice * item.itemQuantity;
+		});
 
-	// 	setTotal(totalPrice);
-	// }, [cart]);
+		setTotalItemPrice(sumTotalItemPrice);
+	}, [shoppingCart]);
 
 	return (
 		<div className='main__cart'>
 			<h3>Shopping Cart</h3>
 			<ul className='cart__list'>
-				{shoppingCart.map(
-					({ itemId, itemTitle, itemPrice, itemImage, itemQuantity }) => {
-						return (
-							<CartItem
-								key={itemId}
-								itemId={itemId}
-								itemTitle={itemTitle}
-								itemPrice={itemPrice}
-								itemImage={itemImage}
-								itemQuantity={itemQuantity}
-							/>
-						);
-					},
-				)}
-
-				{/* {cart.length === 0 ? (
-					<p>No items in the cart</p>
-				) : (
-					<div>
-						{cart.map((item, index) => {
+				{shoppingCart.length > 0 ? (
+					shoppingCart.map(
+						({ itemId, itemTitle, itemPrice, itemImage, itemQuantity }) => {
 							return (
 								<CartItem
-									key={index}
-									id={item.cartId}
-									title={item.cartTitle}
-									price={item.cartPrice}
-									image={item.cartImage}
-									quantity={item.amount}
-									cart={cart}
-									setCart={setCart}
+									key={itemId}
+									itemId={itemId}
+									itemTitle={itemTitle}
+									itemPrice={itemPrice}
+									itemImage={itemImage}
+									itemQuantity={itemQuantity}
+									shoppingCart={shoppingCart}
+									setShoppingCart={setShoppingCart}
 								/>
 							);
-						})}
-					</div>
-				)} */}
+						},
+					)
+				) : (
+					<h1>Empty Cart</h1>
+				)}
 			</ul>
 			<div className='cart__bottom'>
 				<p className='cart__total'>Total:</p>
-				<p className='cart__total_price'>€{total}</p>
+				<p className='cart__total_price'>€{totalItemPrice}</p>
 			</div>
 			<Button bordered color='warning' auto>
 				Checkout
